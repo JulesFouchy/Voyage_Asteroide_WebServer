47a8bcd682fcdadfac9ee83e2c3c74f0f247240c
Jules Fouchy <jules.fouchy@ntymail.com>
2025-04-06 14:00:58 +0200

    Votes

--- server.js
+++ server.js
@@ -8,21 +8,71 @@ const wss = new WebSocket.Server({ server })
 
 app.use(express.static("public")) // Serve static files
 
+let votes = {}
+
+let next_id = 0
+
+let is_voting = false
+
 wss.on("connection", (ws) => {
   console.log("A new client connected")
+  ws.my_id = next_id
+  next_id++
 
   ws.on("message", (message) => {
+    message = Number(message)
     console.log(`Received: ${message}`)
+    if (message === "get") {
+      // Send back the vote summary as JSON
+      ws.send(
+        JSON.stringify({
+          type: "vote_counts",
+          left,
+          right,
+        })
+      )
 
-    // Broadcast message to all clients
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message)
-      }
-    })
+      return
+    }
+    if (message != 0 && message != 1 && message != -1) return
+    votes[ws.my_id] = message
+    console.log(votes)
   })
 
-  ws.on("close", () => console.log("Client disconnected"))
+  ws.on("close", () => {
+    console.log("Client disconnected")
+    delete votes[ws.my_id]
+  })
+})
+
+app.get("/get_votes", (req, res) => {
+  let left = 0
+  let right = 0
+
+  for (const clientId in votes) {
+    const direction = votes[clientId]
+    if (direction === -1) left++
+    if (direction === 1) right++
+  }
+
+  res.json({ left, right })
+})
+
+app.get("/start_game", (req, res) => {
+  is_voting = true
+  votes = {}
+  console.log("Starting game")
+})
+
+app.get("/start_vote", (req, res) => {
+  is_voting = true
+  votes = {}
+  console.log("Starting vote")
+})
+app.get("/stop_vote", (req, res) => {
+  is_voting = false
+  votes = {}
+  console.log("Stopping vote")
 })
 
 server.listen(3000, () =>
